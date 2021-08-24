892f8a7d851fe5e784e3ba97e76de2f6ac676383
RosenDzhekelov <Rosendzhekelow@gmail.com>
2021-08-24 13:31:13 +0300

    Added more solved exercises.

---
+++ MultiDimensionalArraysExercise/07.Test/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.KnightsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                string rows = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rows[col];
+                }
+            }
+
+            bool isValid = false;
+            int removedKnights = 0;
+            while (!isValid)
+            {
+                int currentCount = 0;
+                int bestCounter = 0;
+                int bestRow = 0;
+                int bestCol = 0;
+                bool isContained = false;
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                       
+                        if (matrix[row, col] == 'K')
+                        {
+                            if ( row - 2 >= 0  )
+                            {
+                                if (col - 1 >= 0)
+                                {
+                                    if (matrix[row - 2, col - 1] == 'K')
+                                    {
+                                        currentCount++;
+                                    }
+                                }
+                                if (col + 1 < n)
+                                {
+                                    if (matrix[row - 2, col + 1] == 'K')
+                                    {
+                                        currentCount++;
+                                    }
+                                }
+                            }
+                            if ( row - 1 >= 0 )
+                            {
+                                if (col - 2 >= 0)
+                                {
+                                    if (matrix[row - 1, col - 2] == 'K')
+                                    {
+                                        currentCount++;
+                                    }
+                                }
+                                if (col + 2 < n)
+                                {
+                                    if (matrix[row - 1, col + 2] == 'K')
+                                    {
+                                        currentCount++;
+                                    }
+                                }
+                            }
+                            if ( row + 2 < n )
+                            {
+                                if (col + 1 < n)
+                                {
+                                    if (matrix[row + 2, col + 1] == 'K')
+                                    {
+                                        currentCount++;
+                                    }
+                                }
+                                if (col - 1 >= 0)
+
+                                {
+                                    if (matrix[row + 2, col - 1] == 'K')
+                                    {
+                                        currentCount++;
+                                    }
+                                }
+                            }
+                            if ( row + 1 < n )
+                            {
+                                if (col + 2 < n)
+                                {
+                                    if (matrix[row + 1, col + 2] == 'K')
+                                    {
+                                        currentCount++;
+                                    }
+                                }
+                                if (col - 2 >= 0)
+                                {
+                                    if (matrix[row + 1, col - 2] == 'K')
+                                    {
+                                        currentCount++;
+                                    }
+                                }
+                            }
+                            if(currentCount>bestCounter)
+                            {
+                                bestCol = col;
+                                bestRow = row;
+                                bestCounter = currentCount;
+                                isContained = true;
+                            }
+                            currentCount = 0;
+                        }
+                    }
+                }
+                if(!isContained)
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    matrix[bestRow, bestCol] = '0';
+                    removedKnights++;
+                }
+            }
+            Console.WriteLine(removedKnights);
+        }
+
+
+    }
+}
---
+++ MultiDimensionalArraysExercise/08.Bombs/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                int[] rows = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rows[col];
+                }
+            }
+            int[] coordinates = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < coordinates.Length - 1; i += 2)
+            {
+                int currentCol = coordinates[i + 1];
+                int currentRow = coordinates[i];
+                bool isBoom = false;
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        if (row == currentRow && col == currentCol && matrix[row,col]>0)
+                        {
+                            if (col - 1 >= 0 && matrix[row, col] > 0)
+                            {
+                                matrix[row, col - 1] -= matrix[row, col];
+
+                            }
+                            if (row - 1 >= 0)
+                            {
+                                if (matrix[row - 1, col] > 0)
+                                {
+                                    matrix[row - 1, col] -= matrix[row, col];
+                                }
+                                if (col - 1 >= 0 && matrix[row - 1, col - 1] > 0)
+                                {
+                                    matrix[row - 1, col - 1] -= matrix[row, col];
+                                }
+                                if (col + 1 < n && matrix[row - 1, col + 1] > 0)
+                                {
+                                    matrix[row - 1, col + 1] -= matrix[row, col];
+                                }
+                            }
+                            if (col + 1 < n && matrix[row, col + 1] > 0)
+                            {
+                                matrix[row, col + 1] -= matrix[row, col];
+                            }
+                            if (row + 1 < n)
+                            {
+                                if (matrix[row + 1, col] > 0)
+                                {
+                                    matrix[row + 1, col] -= matrix[row, col];
+                                }
+                                if (col + 1 < n && matrix[row + 1, col + 1] > 0)
+                                {
+                                    matrix[row + 1, col + 1] -= matrix[row, col];
+                                }
+                                if (col - 1 >= 0 && matrix[row + 1, col - 1] > 0)
+                                {
+                                    matrix[row + 1, col - 1] -= matrix[row, col];
+                                }
+                            }
+                            matrix[row, col] = 0;
+                            isBoom = true;
+                            break;
+                        }
+                    }
+                    if (isBoom)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            int count = 0;
+            int sum = 0;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        count++;
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine($"Alive cells: {count}");
+            Console.WriteLine($"Sum: {sum}");
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
