06d2be504a60cf4e8f42d5a90efc12db40bee9e9
RosenDzhekelov <Rosendzhekelow@gmail.com>
2021-09-20 16:54:22 +0300

    lab

---
+++ GenericsLab/BoxOfT/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private readonly Stack<T> elements;
+        public int Count
+        {
+            get
+            {
+                return elements.Count;
+            }
+        }
+        public Box()
+        {
+            elements = new Stack<T>();
+        }
+        public void Add(T element)
+        {
+            elements.Push(element);
+        }
+        public T Remove()
+        {
+           return elements.Pop();
+        }
+    }
+}
---
+++ GenericsLab/GenericScale/EqualityScale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+   public class EqualityScale<T> where T : IComparable
+    {
+        private readonly T right;
+        private readonly T left;
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        public bool AreEqual()
+        {
+            return left.Equals(right);
+        }
+    }
+}
