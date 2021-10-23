48172f1a7daf0286b8a1caab84a43e319aaa9853
RosenDzhekelov <Rosendzhekelow@gmail.com>
2021-10-23 13:54:48 +0300

    dd

---
+++ 03. Stock Market_Skeleton/Skeleton/StockMarket/Investor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        private List<Stock> portfolio;
+        public string FullName { get; set; }
+        public string EmailAdress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public int Count => portfolio.Count;
+        public string BrokerName { get; set; }
+        public Investor(string fullName,string emailAdress , decimal moneyToInvest , string brokerName)
+        {
+            FullName = fullName;
+            EmailAdress = emailAdress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+            portfolio = new List<Stock>();
+        }
+        
+        public void BuyStock(Stock stock)
+        {
+            if(stock.MarketCapitalization>10000 && MoneyToInvest>=stock.PricePerShare)
+            {
+                MoneyToInvest -= stock.PricePerShare;
+                portfolio.Add(stock);
+            }
+        }
+        public string SellStock(string companyName , decimal sellPrice)
+        {
+            Stock stock = portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+            if(stock== null)
+            {
+                return $"{companyName} does not exist.";
+            }
+            if(sellPrice<stock.PricePerShare)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+            portfolio.Remove(stock);
+            MoneyToInvest += sellPrice;
+            return $"{companyName} was sold.";
+        }
+
+        public Stock FindStock(string companyName)
+            => portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+
+        public Stock FindBiggestCompany()
+            => portfolio.OrderByDescending(s => s.MarketCapitalization).FirstOrDefault();
+
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            foreach (var stock in portfolio)
+            {
+                sb.AppendLine(stock.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ 03. Stock Market_Skeleton/Skeleton/StockMarket/StartUp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace StockMarket
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // Sample Code Usage:
+
+            // Initialize Investor
+            Investor investor = new Investor("Peter Lynch", "p.lynch@gmail.com", 2000m, "Fidelity");
+
+            // Initialize Stock
+            Stock ibmStock = new Stock("IBM", "Arvind Krishna", 138.50m, 5000);
+
+            // Print a stock
+            Console.WriteLine(ibmStock.ToString());
+
+            // Company: IBM
+            // Director: Arvind Krishna
+            // Price per share: $138.50
+            // Market capitalization: $692500.00
+
+            // Buy a stock
+            investor.BuyStock(ibmStock);
+
+            // Sell a stock
+            Console.WriteLine(investor.SellStock("IBM", 150.00m)); // "IBM was sold."
+                                                                   // Add stocks
+            Stock teslaStock = new Stock("Tesla", "Elon Musk", 1999.05m, 130);
+            Stock amazonStock = new Stock("Amazon", "Jeff Bezos", 3457.17m, 8500);
+            Stock twitterStock = new Stock("Twitter", "Jack Dorsey", 25, 5000);
+            Stock blizzardStock = new Stock("Activision Blizzard", "Bobby Kotick", 78.53m, 12000);
+
+            // Buy more stocks
+            investor.BuyStock(teslaStock);
+            investor.BuyStock(amazonStock);
+            investor.BuyStock(twitterStock);
+            investor.BuyStock(blizzardStock);
+            Console.WriteLine(investor.SellStock("Tesla",2500m));
+            investor.BuyStock(amazonStock);
+            investor.BuyStock(twitterStock);
+            investor.BuyStock(blizzardStock);
+
+            // FindBiggestCompany
+            Console.WriteLine(investor.FindBiggestCompany());
+
+            // Company: Tesla
+            // Director: Elon Musk
+            // Price per share: $743.17
+            // Market capitalization: $4845468.40
+
+            // Print investor information
+            Console.WriteLine(investor.InvestorInformation());
+
+            // The investor Peter Lynch with a broker Fidelity has stocks:
+            // Company: Tesla
+            // Director: Elon Musk
+            // Price per share: $743.17
+            // Market capitalization: $4845468.40
+            // Company: Twitter
+            // Director: Jack Dorsey
+            // Price per share: $59.66
+            // Market capitalization: $668192.00
+            // Company: Activision Blizzard
+            // Director: Bobby Kotick
+            // Price per share: $78.53
+            // Market capitalization: $433485.60
+
+
+        }
+    }
+}
